717a464fec6ba58c0fd3bae42a86ad4582710cf7
Jose Guevara <5410jos@gmail.com>
2024-11-01 16:52:23 -0600

    Se agregan los modelos y las migraciones necesarias

---
+++ app/Models/Party.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Party extends Model
+{
+    /** @use HasFactory<\Database\Factories\PartyFactory> */
+    use HasFactory;
+    protected $guarded = [];
+}
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,9 +14,12 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email')->unique()->nullable();
+            $table->string('citizen_number')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->unsignedBigInteger('role_id');
+            $table->foreign('role_id')->references('id')->on('roles');
             $table->rememberToken();
             $table->timestamps();
         });
